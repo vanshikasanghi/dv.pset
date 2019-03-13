21677d9e238b56b7eb0c21a634daa2346e420fd5
Vanshika <vanshika.sanghi@gmail.com>
2019-03-13 12:11:41 +0530

    Day 6
    To input 10 numbers and find the smallest pair

---
+++ 6.py
@@ -0,0 +1,20 @@
+i = 1
+numbers = []
+while i<11 :
+	num = int(input("Enter Number "+str(i)+" : ")) 
+	numbers.append(num)
+	i = i+1
+
+fsmall = numbers[0] #storing first smallest
+secsmall = numbers[0] #storing second smallest
+
+x = 9
+while x>=0 :
+	if(numbers[x]<fsmall) :
+		secsmall = fsmall
+		fsmall = numbers[x]
+	elif(numbers[x]<secsmall) :
+		secsmall = numbers[x]
+	x = x-1
+print("The pair is : ")
+print(str(fsmall)+" "+str(secsmall))
